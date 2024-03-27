77d716cdad872429ea6ab0e3bab89ec74e502cc4
MatthewPCope <matthew.p.cope@gmail.com>
2024-03-27 12:05:23 -0700

    added delete button

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from "react"
 
 function App() {
 
-  const [habits, setHabits ] = useState([])
-  const [newHabit, setNewHabit ] = useState('')
-  
+  const [habits, setHabits] = useState([])
+  const [newHabit, setNewHabit] = useState('')
+
   const addHabit = (title) => {
     setHabits((currentHabits) => {
       return [
-        ...currentHabits, { id:crypto.randomUUID(), title}
+        ...currentHabits, { id: crypto.randomUUID(), title}
       ]
     })
   }
@@ -20,37 +20,49 @@ function App() {
     setNewHabit('')
   }
 
-  const changeHandler =(e) => {
+  const changeHandler = (e) => {
     setNewHabit(e.target.value)
+  }
+
+  const deleteHabit = (id) => {
+    setHabits((currentHabits) => {
+      return(
+        currentHabits.filter(habit => habit.id != id)
+      )
+    })
   }
 
   return (
     <>
       <div className=" container mt-5">
-        <form onSubmit={submitHandler}>
-          <div className="form-group">
-          <label className="form-label">Add Habit:
-            <input 
-              type="text"
-              value={newHabit}
-              onChange={changeHandler}
-              id="habit"
-              className="form-control"
-            />
-          </label>
-          <button className="ms-4">Add</button>
-          </div>
-        </form>
-        <ul>
-          {habits.map((habit, index) => {
-            return(
-              <li key={index}>
-                {habit.title}
-              </li>
-            )
-          })}
-        </ul>
+          <form onSubmit={submitHandler}>
+            <div className="form-group">
+            <label className="form-label">Add Habit:
+              <input 
+                type="text"
+                value={newHabit}
+                onChange={changeHandler}
+                id="habit"
+                className="form-control mt-1"
+              />
+            </label>
+            <button className="ms-4 bg-warning rounded">Add</button>
+            </div>
+          </form>
       </div>
+        <div className="text-center">
+          
+            {habits.map((habit) => {
+              return(
+                <li key={habit.id}>
+                  {habit.title}
+                  <button className="ms-2 rounded bg-danger" onClick={() => deleteHabit(habit.id)}>Delete</button>
+                </li>
+              )
+            })}
+          
+        </div>
+      
     </>
   )
 }
